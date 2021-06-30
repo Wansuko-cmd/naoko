4abb1ae6dd40dac37c731e6d895c7ad80671cf81
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-07-01 08:45:21 +0900

    [add] コメントを追加

--- lib/src/main/kotlin/naoko/exception/NaokoException.kt
+++ lib/src/main/kotlin/naoko/exception/NaokoException.kt
@@ -4,6 +4,7 @@ package naoko.exception
 /**
  * message: 考えられるエラー原因
  * response: News API側が返してきたJson
+ * status: エラーの内容
  */
 class NaokoException(
     override val message: String,
@@ -12,15 +13,19 @@ class NaokoException(
     ) : Exception()
 
 enum class NaokoExceptionStatus {
-    RESPONSE_400,
-    RESPONSE_401,
-    RESPONSE_429,
-    RESPONSE_500,
 
+    //Enumのserializer関連
     SERIALIZER_CATEGORY,
     SERIALIZER_COUNTRY,
     SERIALIZER_LANGUAGE,
     SERIALIZER_SORTBY,
 
+    //HTTP通信関連
+    RESPONSE_400,
+    RESPONSE_401,
+    RESPONSE_429,
+    RESPONSE_500,
+
+    //分類されない（HTTP通信部分）
     UNDEFINED
 }
